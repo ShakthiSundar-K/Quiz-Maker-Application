c46c56fffa6f15f2219924e7657995d9de36c9b4
ShakthiSundar-K <shakthisundar01@gmail.com>
2024-12-18 17:27:20 +0530

    updated hooks, utils and service

---
+++ frontend/src/hooks/useLogout.jsx
@@ -0,0 +1,12 @@
+import { useNavigate } from "react-router-dom";
+import toast from "react-hot-toast";
+function useLogout() {
+  let navigate = useNavigate();
+  return () => {
+    sessionStorage.clear();
+    toast.success("Logout Successfull");
+    navigate("/");
+  };
+}
+
+export default useLogout;
